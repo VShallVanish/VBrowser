b0576fd0e4be485f4f9193f5fdf3710ed4e25b91
V <vmattr2@uic.edu>
2024-10-13 16:38:30 -0500

    Finished Chapter 3

--- browser/browser.py
+++ browser/browser.py
@@ -1,6 +1,7 @@
 import socket
 import ssl
 import tkinter
+import tkinter.font
 
 
 global WIDTH, HEIGHT, HSTEP, VSTEP
@@ -99,12 +100,20 @@ class URL:
 class Browser:
     def __init__(self):
         self.window = tkinter.Tk()
+        
+        # Make the scroll bar on the right side of the screen and fit it to the window
+        # self.scrollbar = tkinter.Scrollbar(self.window)
+        # self.scrollbar.pack(side = tkinter.RIGHT, fill = tkinter.Y)
+        
+        # Set the title of the window
         self.window.title("VBrowser")
         self.canvas = tkinter.Canvas(
             self.window,
             width=WIDTH,
             height=HEIGHT
         )
+        print("Canvas created")
+        # print(tkinter.font.families())
         
         # Pack and enable resizing on the canvas
         self.canvas.pack(fill = tkinter.BOTH, expand = True)
@@ -112,10 +121,12 @@ class Browser:
         self.window.bind("<Configure>", self.resize)
         
         self.scroll = 0
-        self.window.bind("<Down>", self.scrollDown)
-        self.window.bind("<Up>", self.scrollUp)
-        self.window.bind("<Button-4>", self.scrollUp)
-        self.window.bind("<Button-5>", self.scrollDown)
+        self.window.bind("<Down>", lambda e: self.scrollDown(e))
+        self.window.bind("<Up>", lambda e: self.scrollUp(e))
+        self.window.bind("<Button-4>", lambda e: self.scrollUp(e))
+        self.window.bind("<Button-5>", lambda e: self.scrollDown(e))
+
+        self.display_list = []
         self.SCROLLSTEP = 100
         self.pageText = ""
         self.width, self.height = WIDTH, HEIGHT
@@ -124,7 +135,7 @@ class Browser:
     def resize(self, event):
         if (self.width, self.height) != (event.width, event.height):
             self.width, self.height = event.width, event.height
-            self.display_list = layout(self.pageText, self.width, self.height)
+            self.display_list = Layout(self.pageText).display_list
             self.draw()
         
     def load(self, url):
@@ -136,20 +147,17 @@ class Browser:
         
         self.pageText = lex(body)
         
-        self.display_list = layout(self.pageText, self.width, self.height)
+        self.display_list = Layout(self.pageText).display_list
         self.draw()
                 
     def draw(self):
         self.canvas.delete("all")
         HEIGHT = self.height
         WIDTH = self.width
-        for x, y, c in self.display_list:
+        for x, y, c, f in self.display_list:
             if y > self.scroll + HEIGHT: continue
-            if y + VSTEP < self.scroll: continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
-            
-        # Draw a scrollbar
-        
+            if y + f.metrics("linespace") < self.scroll: continue
+            self.canvas.create_text(x, y - self.scroll, text=c, font=f, anchor="nw")
             
     def scrollDown(self, e):
         self.scroll += self.SCROLLSTEP
@@ -160,54 +168,128 @@ class Browser:
             self.scroll -= self.SCROLLSTEP
         self.draw()
 
-def layout(text, width, height):
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    
-    for c in text:
-        if c == "\n":
-            cursor_x = HSTEP
-            cursor_y += VSTEP
-        elif c == "\t":
-            cursor_x += HSTEP * 4
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
+class Text:
+    def __init__(self, text):
+        self.text = text
         
-        if cursor_x >= width - HSTEP:
-            cursor_x = HSTEP
-            cursor_y += VSTEP
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
+class Layout:
+    def __init__(self, tokens):
+        self.line = []
+        self.display_list = []
+        self.tokens = tokens
         
-    return display_list
+        self.cursor_x = HSTEP
+        self.cursor_y = VSTEP
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 12
+        
+        for tok in tokens:
+            self.token(tok)
+            
+        self.flush()
     
+    def token(self, tok):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+                 
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4  
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+        
+    
+    def word(self, word):
+        font = get_font(self.size, self.weight, self.style)
+        w = font.measure(word)
+        
+        if self.cursor_x + w > WIDTH - HSTEP:
+            self.flush()
+        self.line.append((self.cursor_x, word, font))
+        self.cursor_x += w + font.measure(" ")
+        
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        
+        baseline = self.cursor_y + max_ascent * 1.25
+        
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + max_descent * 1.25
+        self.cursor_x = HSTEP
+        self.line = []
+        
+FONTS = {}
+    
+def get_font(size, weight, style):
+    key = (size, weight, style)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=style)
+        label = tkinter.Label(font=font)
+        FONTS[key] = (font, label)
+    return FONTS[key][0]
+          
 def lex(body):
-    text = ""
+    out = []
+    buffer = ""
     in_tag = False
     in_entity = False
-    entity = ""
     for c in body:
         if c == "<":
             in_tag = True
+            if buffer:
+                out.append(Text(buffer))
+            buffer = ""
         elif c == ">":
             in_tag = False
+            out.append(Tag(buffer))
+            buffer = ""
         elif c == "&":
             in_entity = True
-            entity = ""
+            if buffer:
+                out.append(Text(buffer))
+            buffer = ""
         elif c == ";":
             in_entity = False
-            if entity == "lt":
-                text += "<"
-            elif entity == "gt":
-                text += ">"
+            if buffer == "lt":
+                buffer += "<"
+            elif buffer == "gt":
+                buffer += ">"
             else:
-                text += "&" + entity + ";"
-            entity = ""
-            
-        elif in_entity:
-            entity += c
-        elif not in_tag and not in_entity:
-            text += c
-            
-    return text
+                buffer += "&" + buffer + ";"
+            buffer = ""
+        else:
+            buffer += c
+    if buffer:
+        if not in_tag and not in_entity:
+            out.append(Text(buffer))
+    return out
         
         
 if __name__ == "__main__":
